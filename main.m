f0d99b8a299b0bb349ccc9d97559ddd7da676ad4
Xiwei-Wang <xiweiwang@live.cn>
2019-07-06 00:55:08 +0800

    Carry out main script mainly

--- main.m
+++ main.m
@@ -5,4 +5,25 @@
 %Initializing
 name='result.csv';
 file=fopen(name,'w');
-fprintf(file,",\r\n");
+fprintf(file,'Switch,f_switch,I_amplitude,Alpha,N_num1,N_num2,P_S_switch_25,P_S_conduct,Eta_25,\r\n');
+
+%Configuring the kind of switches and the step of I for interpolation
+switch_kind=["1700","3300","SIC1200","SIC1700"];
+n=length(switch_kind);
+switch_voltage=[1700,3300,1200,1700];
+I_step=1;%A
+
+%Enumerating
+for i=1:1:n
+    %Interpolating
+    interpolating(switch_kind(i),I_step);
+    %Calculating
+    for f_switch=900:50:3600%Hz
+        for I_amplitude=1:-0.1:0.2%Percentage
+            for Alpha=-10:1:10%Angle, but radian is required by calculation.m
+                [N_num1,N_num2,P_S_switch_25,P_S_conduct,Eta_25]=calculation(f_switch,I_amplitude,Alpha/180*pi,switch_voltage,I_step);%Angle is converted to radian
+                fprintf(file,"%s,%d,%f,%d,%d,%d,%f,%f,%f,\r\n",Switch,f_switch,I_amplitude,Alpha,N_num1,N_num2,P_S_switch_25,P_S_conduct,Eta_25);
+            end
+        end
+    end
+end
