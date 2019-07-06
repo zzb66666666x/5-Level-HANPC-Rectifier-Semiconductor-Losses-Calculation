95a669b1bfdb1ba696b41687d07adc7e65477b85
Xiwei-Wang <xiweiwang@live.cn>
2019-07-06 13:56:24 +0800

    Carry out main script mainly

--- main.m
+++ main.m
@@ -2,6 +2,12 @@
 %Current ripple and DT are not considered.
 %All variables are in the form of SI Unit.
 
+%Defining global variables
+global Es_on_25;
+global Es_off_25;
+global Vds_18;
+global Vsd_18;
+
 %Initializing
 name='result.csv';
 file=fopen(name,'w');
@@ -21,7 +27,7 @@ for i=1:1:n
     for f_switch=900:50:3600%Hz
         for I_amplitude=1:-0.1:0.2%Percentage
             for Alpha=-10:1:10%Angle, but radian is required by calculation.m
-                [N_num1,N_num2,P_S_switch_25,P_S_conduct,Eta_25]=calculation(f_switch,I_amplitude,Alpha/180*pi,switch_voltage,I_step);%Angle is converted to radian
+                [N_num1,N_num2,P_S_switch_25,P_S_conduct,Eta_25]=calculation(f_switch,I_amplitude,Alpha/180*pi,switch_voltage(i),I_step);%Angle is converted to radian
                 fprintf(file,"%s,%d,%f,%d,%d,%d,%f,%f,%f,\r\n",Switch,f_switch,I_amplitude,Alpha,N_num1,N_num2,P_S_switch_25,P_S_conduct,Eta_25);
             end
         end
