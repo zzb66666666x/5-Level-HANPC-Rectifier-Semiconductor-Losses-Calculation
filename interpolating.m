891abfa89bd2335ad08e9936f68b907091b4abc6
Xiwei-Wang <xiweiwang@live.cn>
2019-07-07 14:21:13 +0800

    Change to use method 'linear' for interpolation

--- interpolating.m
+++ interpolating.m
@@ -13,20 +13,20 @@ function interpolating(switch_name,I_step)
     %Interpolating Es_on_25
     name_Es_on_25="DATA/EON"+switch_name+".txt";
     data_Es_on_25=importdata(name_Es_on_25);%First column, I, A; Second column, E, mJ
-    Es_on_25=interp1(data_Es_on_25(:,1),data_Es_on_25(:,2)/1e3,I_interp,'spline','extrap');
+    Es_on_25=interp1(data_Es_on_25(:,1),data_Es_on_25(:,2)/1e3,I_interp,'linear','extrap');
     
     %Interpolating Es_off_25
     name_Es_off_25="DATA/EOFF"+switch_name+".txt";
     data_Es_off_25=importdata(name_Es_off_25);%First column, I, A; Second column, E, mJ
-    Es_off_25=interp1(data_Es_off_25(:,1),data_Es_off_25(:,2)/1e3,I_interp,'spline','extrap');
+    Es_off_25=interp1(data_Es_off_25(:,1),data_Es_off_25(:,2)/1e3,I_interp,'linear','extrap');
     
     %Interpolating Vds_18
     name_Vds_18="DATA/VDS"+switch_name+".txt";
     data_Vds_18=importdata(name_Vds_18);%First column, I, A; Second column, V, V
-    Vds_18=interp1(data_Vds_18(:,1),data_Vds_18(:,2),I_interp,'spline','extrap');
+    Vds_18=interp1(data_Vds_18(:,1),data_Vds_18(:,2),I_interp,'linear','extrap');
     
     %Interpolating Vsd_18
     name_Vsd_18="DATA/VSD"+switch_name+".txt";
     data_Vsd_18=importdata(name_Vsd_18);%First column, I, A; Second column, V, V
-    Vsd_18=interp1(data_Vsd_18(:,1),data_Vsd_18(:,2),I_interp,'spline','extrap');
+    Vsd_18=interp1(data_Vsd_18(:,1),data_Vsd_18(:,2),I_interp,'linear','extrap');
 end
