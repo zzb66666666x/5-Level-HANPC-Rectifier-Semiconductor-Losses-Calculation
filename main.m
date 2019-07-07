3a38488b1ab4edae3600e9f0f6edcb92306eb77a
zzb66666666x <18767066589@163.com>
2019-07-07 17:34:21 +0800

    I added Po to the output and then print it to the excel file.

--- main.m
+++ main.m
@@ -11,7 +11,7 @@ global Vsd_18;
 %Initializing
 name='result.csv';
 file=fopen(name,'w');
-fprintf(file,'Switch,f_switch,I_amplitude,Alpha,N_num1,N_num2,P_S_switch_25,P_S_conduct_25,Eta_25,\r\n');
+fprintf(file,'Switch,f_switch,I_amplitude,Alpha,N_num1,N_num2,Po,P_S_switch_25,P_S_conduct_25,Efficiency,\r\n');
 
 %Configuring the kind of switches and the step of I for interpolation
 switch_kind=["1700","3300","SIC1200","SIC1700"];
@@ -20,16 +20,17 @@ switch_voltage=[1700,3300,1200,1700];
 I_step=0.1;%A
 
 %Enumerating
-for i=1:1:n
+for i=1:1:n %For temporary.
     %Interpolating
     interpolating(switch_kind(i),I_step);
     %Calculating
     for f_switch=900:50:3600%Hz
         for I_amplitude=1:-0.1:0.2%Percentage
             for Alpha=-10:1:10%Angle, but radian is required by calculation.m
-                [N_num1,N_num2,P_S_switch_25,P_S_conduct_25,Eta_25]=calculation(f_switch,I_amplitude,Alpha/180*pi,switch_voltage(i),I_step);%Angle is converted to radian
-                fprintf(file,"%s,%d,%f,%d,%d,%d,%f,%f,%f,\r\n",switch_kind(i),f_switch,I_amplitude,Alpha,N_num1,N_num2,P_S_switch_25,P_S_conduct_25,Eta_25);
+                [N_num1,N_num2,Po,P_S_switch_25,P_S_conduct_25,Eta_25]=calculation(f_switch,I_amplitude,Alpha/180*pi,switch_voltage(i),I_step);%Angle is converted to radian
+                fprintf(file,"%s,%d,%f,%d,%d,%d,%f,%f,%f,%f,\r\n",switch_kind(i),f_switch,I_amplitude,Alpha,N_num1,N_num2,Po,P_S_switch_25,P_S_conduct_25,Eta_25);
             end
         end
     end
 end
+fclose(file);
